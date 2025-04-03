8ab256e2dd031ff92a6a4800bf0d3b62dc3bc456
Rhishi kesh <reshikash300@gmail.com>
2025-04-04 00:57:55 +0600

    Make Get Stard Btn Dynamic & create hero section

---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HeroImage from '@/assets/image/image4.png'
+import GetStardBtn from '@/sm_components/GetStardBtn.vue';
+</script>
+
+<template>
+    <div class="container mx-auto">
+        <div class="text-center">
+            <h2>A QR code-powered photo album that lets guests effortlessly upload and
+                share memories in one private, organized space.
+            </h2>
+
+            <p>Millions of wedding photos stay unseen in camera rolls, and even the best photographers can miss the most
+                heartfelt moments.</p>
+
+            <p>Our platform lets you effortlessly collect and share every memory, ensuring nothing is lost
+                and the moments that matter most are captured forever.</p>
+
+            <h2>No App Needed!</h2>
+
+            <div>
+                <GetStardBtn :icon="false" />
+            </div>
+
+            <img :src="HeroImage" alt="" class="w-full">
+        </div>
+    </div>
+</template>
---
+++ src/sm_components/GetStardBtn.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    icon: {
+        type: Boolean,
+        default: true
+    }
+});
+</script>
+
+<template>
+    <div class="flex justify-center items-center gap-2">
+        <a href="" class="flex justify-center items-center gap-1 bg-[var(--secondary-color)] px-5 py-2 rounded-3xl">
+            <span class="block">Get Started</span>
+            <svg v-if="icon" class="block" xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16"
+                fill="none">
+                <path
+                    d="M11.4385 6.276L5.70048 12.014L4.75781 11.0713L10.4951 5.33333H5.43848V4H12.7718V11.3333H11.4385V6.276Z"
+                    fill="white" />
+            </svg>
+        </a>
+
+        <a v-if="!icon" href="" class="flex justify-center items-center bg-[var(--secondary-color)] p-3 rounded-full">
+            <svg class="block" xmlns="http://www.w3.org/2000/svg" width="17" height="16" viewBox="0 0 17 16"
+                fill="none">
+                <path
+                    d="M11.4385 6.276L5.70048 12.014L4.75781 11.0713L10.4951 5.33333H5.43848V4H12.7718V11.3333H11.4385V6.276Z"
+                    fill="white" />
+            </svg>
+        </a>
+    </div>
+</template>
